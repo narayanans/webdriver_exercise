d3117faeb38a8dd4160f07f912e20088ecb44d4f
narayanans <narayanan.srinivasan@outlook.com>
2015-07-26 22:03:51 -0500

    Changed ChromeDriver to FirefoxDriver

---
+++ README.md
@@ -0,0 +1,10 @@
+# webdriver_exercise
+
+The exercise is implemented using junit and pageobject model. Tests are configured to run on firefox by default. The browser start up code is present in BaseTest.java which is inherited by every test.
+
+Instructions to run:
+This exercise uses maven for dependency and build management. 
+mvn test will execute the tests.
+mvn surefire-report:report-only will create a surefire report.
+
+  
--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -5,25 +5,25 @@ import java.util.concurrent.TimeUnit;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BaseTest {
 
 	WebDriver driver;
-	
+
 	@Before
-	public void setUp(){
-		System.out.println("Base test set up");
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\naray\\chromedriver_win32\\chromedriver.exe");
-		driver = new ChromeDriver();
+	public void setUp() {
+		// System.setProperty("webdriver.chrome.driver",
+		// "C:\\Users\\naray\\chromedriver_win32\\chromedriver.exe");
+		// driver = new ChromeDriver();
+		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
 		driver.navigate().to("http://store.demoqa.com/");
 	}
-	
+
 	@After
-	public void tearDown(){
-		System.out.println("Base test tear down");
+	public void tearDown() {
 		driver.quit();
 	}
-	
+
 }
